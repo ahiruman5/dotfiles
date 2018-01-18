55a371e5ec60f9fb394b087ff886561cd2f36008
ahiruman5 <ahiruman5@gmail.com>
2018-01-18 09:30:30 +0900

    setup.shの文言修正

--- setup.sh
+++ setup.sh
@@ -1,44 +1,44 @@
 #!/bin/sh
 
-#setup.shを置いてあるパスをCURRENTに代入
+# dotfilesのPATHを取得
 CURRENT=$(cd $(dirname $0) && pwd)
 
-#setup.shと同じ階層のファイルをホームディレクトリに向けてシンボリックリンクを貼る
+# シンボリックリンク
 ln -sf ${CURRENT}/.bash/.bash_profile ~
 ln -sf ${CURRENT}/.vim/.vimrc ~
 ln -sf ${CURRENT}/.gitconfig ~
 ln -sf ${CURRENT}/.agignore ~
 ln -sf ${CURRENT}/.dir_colors ~
+ln -sf ${CURRENT}/bin/git_diff_wrapper ~/bin/
+ln -sf ${CURRENT}/.zsh/.zshenv ~/.zsh/
+ln -sf ${CURRENT}/.zsh/.zshrc ~/.zsh/
+ln -sf ${CURRENT}/.peco/config.json ~/.peco/
 
+# ディレクトリ作成
 if [ ! -d ~/.zsh/ ]
 then
     mkdir ~/.zsh/
 fi
-ln -sf ${CURRENT}/.zsh/.zshenv ~/.zsh/
-ln -sf ${CURRENT}/.zsh/.zshrc ~/.zsh/
-
-if [ ! -d ~/.peco/ ]
-then
-    mkdir ~/.peco/
-fi
-ln -sf ${CURRENT}/.peco/config.json ~/.peco/
 
 if [ ! -d ~/bin/ ]
 then
     mkdir ~/bin/
 fi
 
-ln -sf ${CURRENT}/bin/git_diff_wrapper ~/bin/
+if [ ! -d ~/.peco/ ]
+then
+    mkdir ~/.peco/
+fi
 
-#MacとLinuxでそれぞれ別のコマンドを利用
+# OS毎のバイナリ配置
 case ${OSTYPE} in
     darwin*)
-        #Mac用の設定
+        #Mac用のバイナリ
         ln -sf ${CURRENT}/bin/mac/peco ~/bin/
         ln -sf ${CURRENT}/bin/mac/exa ~/bin/
         ;;
     linux*)
-        #Linux用の設定
+        #Linux用のバイナリ
         ln -sf ${CURRENT}/bin/linux/peco ~/bin/
         ln -sf ${CURRENT}/bin/linux/exa ~/bin/
         ln -sf ${CURRENT}/bin/linux/jq ~/bin/
