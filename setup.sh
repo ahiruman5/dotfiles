795dc89156fb9f6015ac2d5a9cc02c491b9962ba
ahiruman5 <ahiruman5@gmail.com>
2018-01-18 11:34:04 +0900

    setup.sh整理

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 # dotfilesのPATHを取得
 CURRENT=$(cd $(dirname $0) && pwd)
 
+# ディレクトリ作成
+if [ ! -d ~/bin/ ]
+then
+    mkdir ~/bin/
+fi
+
 # シンボリックリンク
 ln -sf ${CURRENT}/.bash/.bash_profile ~
 ln -sf ${CURRENT}/.vim/.vimrc ~
@@ -10,35 +16,19 @@ ln -sf ${CURRENT}/.gitconfig ~
 ln -sf ${CURRENT}/.agignore ~
 ln -sf ${CURRENT}/.dir_colors ~
 ln -sf ${CURRENT}/bin/git_diff_wrapper ~/bin/
-ln -sf ${CURRENT}/.zsh/.zshenv ~/.zsh/
-ln -sf ${CURRENT}/.zsh/.zshrc ~/.zsh/
-ln -sf ${CURRENT}/.peco/config.json ~/.peco/
+ln -sf ${CURRENT}/.zsh ~
+ln -sf ${CURRENT}/.peco ~
 
-# ディレクトリ作成
-if [ ! -d ~/.zsh/ ]
-then
-    mkdir ~/.zsh/
-fi
-
-if [ ! -d ~/bin/ ]
-then
-    mkdir ~/bin/
-fi
-
-if [ ! -d ~/.peco/ ]
-then
-    mkdir ~/.peco/
-fi
 
 # OS毎のバイナリ配置
 case ${OSTYPE} in
     darwin*)
-        #Mac用のバイナリ
+        # Mac用のバイナリ
         ln -sf ${CURRENT}/bin/mac/peco ~/bin/
         ln -sf ${CURRENT}/bin/mac/exa ~/bin/
         ;;
     linux*)
-        #Linux用のバイナリ
+        # Linux用のバイナリ
         ln -sf ${CURRENT}/bin/linux/peco ~/bin/
         ln -sf ${CURRENT}/bin/linux/exa ~/bin/
         ln -sf ${CURRENT}/bin/linux/jq ~/bin/
